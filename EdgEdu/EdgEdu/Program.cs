8ef259f206308de963d8fa41baee4c8b96d17782
Sukanya <Sukanyak21130@gmail.com>
2017-08-31 14:23:33 +0700

    year 2560

--- EdgEdu/EdgEdu/Program.cs
+++ EdgEdu/EdgEdu/Program.cs
@@ -10,9 +10,14 @@ namespace EdgEdu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Year 255x semester 1, I am an Engedu freshman.");
-            Console.WriteLine("Year 255x semester 2, I learn to  program in C sharp.");
+            Console.WriteLine("Year 2559 semester 1, I am an Engedu freshman.");
+            Console.WriteLine("Year 2559 semester 2, I learn to  program in C sharp.");
             Console.WriteLine("Year 2560 semester 1, I start my programmer life.");
+            Console.WriteLine("Year 2560 semester 2, I learn database.");
+           
+
+
+
         }
     }
 }
